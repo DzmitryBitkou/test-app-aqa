73366cd0036eff33dd10ba9c4ac57fbdc51977d4
Dzmitry Bitkou <91487122+DzmitryBitkou@users.noreply.github.com>
2026-04-23 23:18:41 +0300

    Add POM locators for WhatsNewLink and Steps dropdown/packages

--- page_object/QavaHomePage.js
+++ page_object/QavaHomePage.js
@@ -1,7 +1,30 @@
 const { locator } = require('@qavajs/steps-playwright/po.js');
 
+// QavaHomePage POM - enhanced with locators used by the whats_new_and_steps.feature
 module.exports = class QavaHomePage {
+    // root selector for the page
     selector = 'body';
+
+    // Existing locators
     Header = locator('header h1');
     WritingTestsLink = locator('a.menu__link[href*="writing-tests"]');
+
+    // "What's new in v2" link - flexible selectors to handle minor markup differences
+    WhatsNewLink = locator('a:has-text("What\'s new in v2"), a:has-text("Whats new in v2"), a[href*="whats-new"], a[href*="whats-new-v2"]');
+
+    // "Steps" dropdown trigger and menu
+    StepsDropdown = locator('a:has-text("Steps"), button:has-text("Steps")');
+    StepsDropdownMenu = locator('nav, .menu, .dropdown, ul.menu__list');
+
+    // Expected package items inside Steps dropdown
+    StepCore = locator('a:has-text("core")');
+    StepApi = locator('a:has-text("api")');
+    StepElement = locator('a:has-text("element")');
+    StepAssert = locator('a:has-text("assert")');
+    StepUtils = locator('a:has-text("utils")');
+
+    // Return list of expected package locators in order
+    expectedStepsLocators() {
+        return [this.StepCore, this.StepApi, this.StepElement, this.StepAssert, this.StepUtils];
+    }
 }
